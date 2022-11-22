fabfe5debe51c407196d9c1804184bd7423de206
Claire <claire.kurth@orange.fr>
2022-11-22 17:46:08 +0100

    Doc constantes.h

--- code/include/constantes.h
+++ code/include/constantes.h
@@ -1,6 +1,12 @@
 #ifndef CONSTANTES_H
 #define CONSTANTES_H
 
+/// @brief Header qui permet de définir un nom pour chaque index de Page du QStackedWiget de MainWindow
+/// @authors Nicolas Robert, Victor Dallé, Claire Kurth
+/// @version 1.1
+
+
+/// @brief Define de l'index de PageLogin
 #define LOGIN_PAGE 0
 
 #endif // CONSTANTES_H
