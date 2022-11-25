8b5d67112594dad3eac25302485088e7e565849b
Victor Dallé <dallevictor@gmail.com>
2022-11-25 12:51:48 +0100

    modif doc et correction fautes

--- code/src/message.cpp
+++ code/src/message.cpp
@@ -2,7 +2,7 @@
 
 
 Message::Message(std::string id_message, std::string date_envoie, Utilisateur *user, Ticket *ticket, std::string message):
-    id_message(id_message), date_envoie(date_envoie), user(user), ticket(ticket), message(message)
+    id_message(id_message), date_envoi(date_envoie), user(user), ticket(ticket), message(message)
 {}
 
 
@@ -16,14 +16,14 @@ void Message::setTicket(Ticket* &value)
     ticket = value;
 }
 
-std::string Message::getDate_envoie() const
+std::string Message::getDate_envoi() const
 {
-    return date_envoie;
+    return date_envoi;
 }
 
-void Message::setDate_envoie(double value)
+void Message::setDate_envoi(double value)
 {
-    date_envoie = value;
+    date_envoi = value;
 }
 
 Utilisateur* Message::getUser() const
@@ -50,7 +50,7 @@ void Message::setId_message(const std::string &value)
 std::string Message::toString(){
     return "Message = {\n\
             \tid: '" + id_message + "',\n\
-            \tdate_creation: '" + date_envoie + "'\n\
+            \tdate_creation: '" + date_envoi + "'\n\
             \tmessage: '" + message + "'\n\
             \tauteur: '" + user->toString() + "'\n}";
 }
