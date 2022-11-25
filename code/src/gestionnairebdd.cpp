8c33f1504608a88a1b964229e68fac8c1524ef68
Claire <claire.kurth@orange.fr>
2022-11-25 19:55:13 +0100

    chemin relative pour le file de la bdd

--- code/src/gestionnairebdd.cpp
+++ code/src/gestionnairebdd.cpp
@@ -3,7 +3,14 @@
 GestionnaireBDD::GestionnaireBDD()
 {
     my_db = QSqlDatabase::addDatabase("QSQLITE");
-    my_db.setDatabaseName("C:/Users/Victor/Documents/Github/EasyTicket/code/resources/database.db");
+    //Récupère le path absolue de l'exécutable
+    QString p = qApp->QCoreApplication::applicationDirPath() ;
+    //On coupe le lien en 2 avec EasyTicket/
+    QRegExp tagExp("EasyTicket/");
+    QStringList firstList = p.split(tagExp);
+    //Ajoute ce qui manque pour arriver au file
+    QString path = firstList.takeFirst() +"EasyTicket/code/resources/database.db";
+    my_db.setDatabaseName(path);
 
     if (!my_db.open())
     {
