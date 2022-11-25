612bce1008b70e46c24af5f6987e4dee3ef8dcfa
Victor Dallé <dallevictor@gmail.com>
2022-11-25 16:33:46 +0100

    ajout de la page principale de la doc

--- code/include/mainwindow.h
+++ code/include/mainwindow.h
@@ -1,6 +1,42 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+/// \mainpage Documentation EasyTicket
+///
+/// \section versions_sec Version
+/// \subsection v1_1_subsec v1.1
+/// Page de connexion avec au moment de la connexion, la création d'un objet Utilisateur.
+///
+/// Peut importe le login et le mot de passe, on affiche un message contenant le 'toString' de notre objet Utilisateur.
+///
+/// \subsection v1_2_subsec v1.2
+/// Une fois connecté avec un compte 'client', l'utilisateur accède à la page d'accueil (PageAccueilClient).
+///
+/// Ajout d'un GestionnaireDialogue qui fait le lien entre l'utilisateur et les données.
+///
+/// \subsection v1_3_subsec v1.3
+/// Le bouton de création de ticket mène à une page vierge qui servira pour créer les tickets (PageAjoutTicket).
+///
+/// Ajout d'un bouton de retour à la page d'accueil quand on est sur la page de création de ticket.
+///
+/// \subsection v1_4_subsec v1.4
+/// Création de la mise en page du formulaire pour créer le ticket.
+///
+/// Affiche le resultat sur la sortie standard et si jamais il y a des champs invalides.
+///
+/// \subsection v1_5_subsec v1.5
+/// Vérification des champs saisis par l'utilisation lors de la création d'un nouveau ticket.
+///
+/// Fonctions de vérifications pour le Message du ticket et génération d'un message contenant le descriptif du Ticket créé (sans encore le message).
+///
+/// \subsection v1_6_subsec v1.6
+/// Implémentation de l'ajout de Message dans le Ticket au moment de sa création. Au moment du clic pour créer le Ticket, on affiche les données du Ticket ainsi que le Message ajouté.
+///
+/// Si tout est ok, on retourne à la page d'accueil (PageAccueilClient).
+///
+/// \subsection v1_7_subsec v1.7
+/// Ajout de la base de donnée au format db avec SQLite.
+
 #include <QMainWindow>
 #include <QStackedWidget>
 #include "pagelogin.h"
